759f482cbd4a645b7501444b7cb1e92b24293b33
rtsuji001 <r.tsuji001@gmail.com>
2018-09-23 00:27:23 +0900

    新しいファイルの追加

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ln -sf ~/git/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/git/dotfiles/dein.toml ~/.config/nvim/dein.toml
+ln -sf ~/git/dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+
